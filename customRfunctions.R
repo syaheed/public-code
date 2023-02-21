83bca53a2df51dc99c21d475af08ff850c84e517
syaheed <syaheed@gmail.com>
2023-02-21 10:27:37 +0800

    Update customRfunctions.R

--- customRfunctions.R
+++ customRfunctions.R
@@ -1,7 +1,10 @@
+# This is just a function list / help page for things that I find myself using quite a bit.
+
 # how to use:
 #library(devtools)
 #source_url("https://raw.githubusercontent.com/syaheed/public-code/master/customRfunctions.R")
 
+# custom functions:
 rowsplit = function(x,column = 0, sp= "_"){
 # splits rows of strings into a matrix based on a seperator (sp), and either returns the whole matrix (column = 0), or a particular column
   temp = do.call(rbind,strsplit(x,sp))
@@ -9,8 +12,14 @@ rowsplit = function(x,column = 0, sp= "_"){
   if(column > 0){return(temp[,column])}
 }
 
+#other how to's:
+
 # how to connect R to a postgresql:
 #library(DBI)
 #con = dbConnect(RPostgres::Postgres(), dbname = 'a', host = 'b', port = 5432, user = 'user', password = 'pass')
 #query = "SELECT * FROM INFORMATION_SCHEMA.TABLES"
 #d = dbGetQuery(con, query)
+
+#read json files
+#library(jsonlite)
+#read_json(file)

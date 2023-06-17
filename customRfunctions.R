bea655e83ec7e49d795fc38715d0a84564b68f0e
syaheed <syaheed@gmail.com>
2023-06-17 09:29:17 +0800

    Update customRfunctions.R

--- customRfunctions.R
+++ customRfunctions.R
@@ -220,6 +220,8 @@ corMap = function(corMat, patternMap = c("a","b","c","d","e")){
 #dbGetQuery(con, "SELECT AVG(SW) FROM data WHERE PW > 0.5 AND  PL > 5.0")
 #dbGetQuery(con, "SELECT AVG(SW)/AVG(SL) FROM data WHERE PW > 0.5 AND  PL > 5.0") #output a computation
 #dbGetQuery(con, "SELECT round(AVG(SL),2) FROM data WHERE PW > 0.5 AND  PL > 5.0") # round off to x dp [negative numbers work too, eg -3 to round off to nearest 1000]
+#dbGetQuery(con, "SELECT length(Species) FROM data WHERE PW > 0.5") #output length of string
+#dbGetQuery(con, "SELECT Species,PW,PL FROM data WHERE PW < (PL/4)") # selection on compute
 
 # NULL case
 #dbGetQuery(con, "SELECT * FROM data WHERE PL is NULL")
